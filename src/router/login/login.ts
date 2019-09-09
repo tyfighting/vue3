6037a71cd5da472a634d2d97439e4448d0d4efd2
tyfighting <13271508702@126.com>
2019-09-09 15:21:36 +0800

    vueRouter

--- src/router/login/login.ts
+++ src/router/login/login.ts
@@ -1,3 +1,21 @@
-export default [{
-  
-}]
+import Home from '@/app.vue';
+export default [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    redirect: '/page1',
+    children: [
+      {
+        path: '/page1',
+        name: 'page1',
+        component: () => import('@/views/home/page1.vue')
+      },
+      {
+        path: '/page2',
+        name: 'page2',
+        component: () => import('@/views/home/page2.vue')
+      }
+    ]
+  }
+];
